bc6fcc367d3a2ce0c20aaddfa0ff9a43965c7b91
JackMilner1 <jackellismilner@gmail.com>
2025-11-16 15:40:38 +0000

    play list collection class creation

--- src/spotifyclone/PlayListCollection.java
+++ src/spotifyclone/PlayListCollection.java
@@ -1,5 +1,34 @@
 package spotifyclone;
+import java.util.ArrayList;
 
 public class PlayListCollection {
+    private ArrayList<PlayList> playLists;
     
+    PlayListCollection() {
+        playLists = new ArrayList<PlayList>();
+    }
+    
+    public void addPlayList(String name) {
+        
+    }
+    
+    public void removePlayList(int index) {
+        
+    }
+    
+    public void duplicatePlayList(String name, int index) {
+        
+    }
+    
+    public ArrayList<PlayList> getPlayLists() {
+        return playLists;
+    }
+    
+    public PlayList getPlayList(int index) {
+        return playLists.get(index);
+    }
+    
+    @Override public String toString () { 
+        return "";
+    }
 }
