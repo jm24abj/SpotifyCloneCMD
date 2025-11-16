4d578ec175fb288a40b4eb0ded07a7d36d958f05
JackMilner1 <jackellismilner@gmail.com>
2025-11-16 15:40:08 +0000

    music library class creation

--- src/spotifyclone/MusicLibrary.java
+++ src/spotifyclone/MusicLibrary.java
@@ -1,5 +1,18 @@
 package spotifyclone;
+import java.util.ArrayList;
 
 public class MusicLibrary {
+    private ArrayList<Song> songs;
     
+    MusicLibrary() {
+        songs = new ArrayList<Song>();
+    }
+    
+    public void addSong(Song song) {
+        songs.add(song);
+    }
+    
+    public ArrayList<Song> search(String title) {
+        return new ArrayList<Song>();
+    }
 }
